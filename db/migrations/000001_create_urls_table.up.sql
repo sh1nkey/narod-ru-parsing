d46fa6f2594152fc9e5a7b3009082da8686764c1
Alexey Kudelko <110509023+sh1nkey@users.noreply.github.com>
2025-02-17 12:55:19 +0300

    Delete db directory

--- db/migrations/000001_create_urls_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
-    url TEXT NOT NULL UNIQUE,
-    description TEXT,
-    is_empty BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT check_is_empty_not_true_when_description_not_empty
-    CHECK (is_empty = FALSE OR description IS NULL OR description = '')
-);
